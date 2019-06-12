92d6dc3fee439571b74454dff566817e51894e6f
Milan Gatyas <12131213@seznam.cz>
2019-06-12 20:25:43 +0200

    apply function added

--- extensions/src/main/java/cz/mg/lib/extensions/AnyExtensions.kt
+++ extensions/src/main/java/cz/mg/lib/extensions/AnyExtensions.kt
@@ -2,4 +2,10 @@ package cz.mg.lib.extensions
 
 import com.google.gson.Gson
 
+fun<T> T.apply(fn: (self: T) -> Unit): T {
+    fn(this)
+
+    return this
+}
+
 fun Any.toJson(): String = Gson().toJson(this)
